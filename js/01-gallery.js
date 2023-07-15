88cf3e76f5a50a5d1063d345f4770e02a284a025
SerhiiHolovk <98563452+SerhiiHolovk@users.noreply.github.com>
2023-07-15 17:11:00 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery')
 
-console.log(galleryItems);
+function createGalleryItem({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="Image ${description}"
+    />
+  </a>
+</li>
+  `;
+}
+
+// Функція для створення всієї розмітки галереї
+function createGalleryMarkup(items) {
+  return items.map(createGalleryItem).join('');
+}
+gallery.innerHTML = createGalleryMarkup(galleryItems);
+
+gallery.addEventListener('click', handlerGalleryClick);
+
+function handlerGalleryClick(event) {
+  event.preventDefault()
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  const instance = basicLightbox.create(
+    `<img width = "1366" height = "768" src = "${event.target.dataset.source}"\>`);
+  instance.show();
+  window.addEventListener("keydown", (event) => {
+    if (event.code === "Escape") {
+      instance.close();
+   }
+  });
+}
